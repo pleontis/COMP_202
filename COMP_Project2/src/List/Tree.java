03cbf511b212852b574182f4b632ae7d9762d786
Panagiotis Leontis <67234862+pleontis@users.noreply.github.com>
2021-03-25 19:15:24 +0200

    Delete COMP_Project2 directory

--- COMP_Project2/src/List/Tree.java
+++
@@ -1,8 +0,0 @@
-package List;
-
-public interface Tree<T> {
-	public void insert(T node);
-	public T search(T start,int key);
-	public int[] inOrder(T start);
-	public void searchByRange(T start,int firstNum,int lastNum);
-}
